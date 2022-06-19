245991423fe46e4d4117ce4de74ef45a3a694aa5
sprhawk <465558+sprhawk@users.noreply.github.com>
2022-06-19 09:41:36 +0800

    added shared prefix and suffix insertion tests

--- tests/fst_tests.cpp
+++ tests/fst_tests.cpp
@@ -16,16 +16,23 @@ TEST_GROUP(SimpleGroup) {
   }
 };
 
-TEST(SimpleGroup, InsertMondayTest) {
+TEST(SimpleGroup, InsertOneTest) {
   fst->insert("mon", 1);
   auto next_id = fst->get_next_id();
   CHECK_COMPARE(next_id, ==, 4);
 }
 
-TEST(SimpleGroup, InsertMoreDaysTest) {
+TEST(SimpleGroup, InsertTwoDaysNoPrefixNoSuffixTest) {
   fst->insert("mon", 1);  
-  fst->insert("tues", 2);
+  fst->insert("thurs", 2);
+  auto next_id = fst->get_next_id();
+  CHECK_COMPARE(next_id, ==, 8);
+}
+
+TEST(SimpleGroup, InsertThreeDaysTest) {
+  fst->insert("mon", 1);  
   fst->insert("thurs", 4);
+  fst->insert("tues", 2);
   auto next_id = fst->get_next_id();
   CHECK_COMPARE(next_id, ==, 9);
 }
